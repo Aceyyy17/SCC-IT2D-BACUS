f3804f86cc3792e410d5b481d580c4b85e892dba
Aceyyy17 <acearcilla1434@gmail.com>
2024-09-10 15:50:23 +0800

    I updated Products

--- src/it2d/bacus/IT2DBACUS.java
+++ src/it2d/bacus/IT2DBACUS.java
@@ -8,15 +8,18 @@ public class IT2DBACUS {
     public static void main(String[] args) {
         
 //       Grades gr = new Grades();
-  //     gr.addGrades("Ace", 101, 3.5, 3.5, 2.5, 3.4);
-  //     gr.viewGrades();
+//     gr.addGrades("Ace", 101, 3.5, 3.5, 2.5, 3.4);
+//     gr.viewGrades();
        
- //      Grades gr1 = new Grades();
-   //    gr1.addGrades("Mark", 102, 2.5, 1.5, 2.5, 2.4);
-  //     gr1.viewGrades();
+//     Grades gr1 = new Grades();
+//     gr1.addGrades("Mark", 102, 2.5, 1.5, 2.5, 2.4);
+//     gr1.viewGrades();
   
-         Grade grd = new Grade();
-         grd.getGradeinput();
+//     Grade grd = new Grade();
+//     grd.getGradeinput();
+
+       Products prod = new Products();
+       prod.products();
     }
     
 }
--- src/it2d/bacus/Products.java
+++ src/it2d/bacus/Products.java
@@ -1,7 +1,53 @@
 
 package it2d.bacus;
 
+import java.util.Scanner;
 
 public class Products {
+    
+    public void products(){
+        Scanner input = new Scanner (System.in);
+            
+        String Cname;
+        String Pname;
+        int qty, price;
+        double cash, due, change;
+        
+        System.out.print("Customer's name: ");
+        Cname = input.nextLine();
+        
+        System.out.print("Product name: ");
+        Pname = input.nextLine();
+        
+        System.out.print("Quantity: ");
+        qty = input.nextInt();
+        
+        System.out.print("Price: ");
+        price = input.nextInt();
+        
+        System.out.print("Cash: ");
+        cash = input.nextInt();
+        
+        
+        
+        System.out.println("\n-------------------------------");
+        System.out.println("RECEIPT");
+        System.out.println("-------------------------------");
+        
+        System.out.println("Name:"+Cname+"\nProduct name: "+Pname+"\nQuantity"+qty);
+        System.out.println("-------------------------------");
+        
+        due = qty*price;
+        System.out.println("Total due: "+ String.format("%.2f",due));
+        System.out.println("'\nCash: "+ String.format("%.2f",cash));
+        
+         System.out.println("-------------------------------");
+         change = cash-due;
+         
+         System.out.println("Change: "+ String.format("%.2f",change));
+        
+        
+        
+    }
     
 }
