316fce885639ec04279208825f43bc029da2eee1
Aceyyy17 <acearcilla1434@gmail.com>
2024-09-10 14:52:08 +0800

    I updated Grades

--- src/it2d/bacus/IT2DBACUS.java
+++ src/it2d/bacus/IT2DBACUS.java
@@ -6,7 +6,17 @@ public class IT2DBACUS {
 
     
     public static void main(String[] args) {
+        
+//       Grades gr = new Grades();
+  //     gr.addGrades("Ace", 101, 3.5, 3.5, 2.5, 3.4);
+  //     gr.viewGrades();
        
+ //      Grades gr1 = new Grades();
+   //    gr1.addGrades("Mark", 102, 2.5, 1.5, 2.5, 2.4);
+  //     gr1.viewGrades();
+  
+         Grade grd = new Grade();
+         grd.getGradeinput();
     }
     
 }
