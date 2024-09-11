0a8160f688a35a00ec6c8d8b7a081552efc0d038
Aceyyy17 <acearcilla1434@gmail.com>
2024-09-11 17:44:40 +0800

    I updated Accounts

--- src/it2d/bacus/Accounts.java
+++ src/it2d/bacus/Accounts.java
@@ -4,4 +4,29 @@ package it2d.bacus;
 
 public class Accounts {
     
+    int id;
+    String Firstname;
+    String Lastname;
+    String Email;
+    String Username;
+    String Password;
+    
+    public void detailsAccount(int uid, String Fn, String Ln, String Eml, String Un, String pass){
+        this.id = uid;
+        this.Firstname = Fn;
+        this.Lastname = Ln;
+        this.Email = Eml;
+        this.Username = Un;
+        this.Password = pass;
+       
+    }
+    
+    public void viewAccount(){
+        System.out.println("ID: "+this.id);
+        System.out.println("Enter First name: "+this.Firstname);
+        System.out.println("Enter Last name: "+this.Lastname);
+        System.out.println("Enter Email:"+this.Email);
+        System.out.println("Enter Username: "+this.Username);
+        System.out.println("Enter Password:"+this.Password);
+    }
 }
--- src/it2d/bacus/IT2DBACUS.java
+++ src/it2d/bacus/IT2DBACUS.java
@@ -18,8 +18,15 @@ public class IT2DBACUS {
 //     Grade grd = new Grade();
 //     grd.getGradeinput();
 
-       Products prod = new Products();
-       prod.products();
+//       Products prod = new Products();
+//       prod.products();
+
+ //      Accounts acc = new Accounts();
+ //      acc.detailsAccount(101,"Ace", "Bacus", "aceyyywelman@gmail.com", "Acey", "Acey1434");
+  //     acc.viewAccount();
+ //      Account acc = new Account();
+ //      acc.personInfo();
+       
     }
     
 }
