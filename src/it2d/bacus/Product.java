4a321c61504af0bbe8b664c5f214941da63ee6ba
Aceyyy17 <acearcilla1434@gmail.com>
2024-09-21 11:29:27 +0800

    I updated products

--- src/it2d/bacus/Product.java
+++ src/it2d/bacus/Product.java
@@ -1,34 +1,32 @@
 package it2d.bacus;
 
-
-
 public class Product {
-     int id;
-     String cName;
-     String pName;
-     int qty;
-     double price;
+    int id;
+    String cName;
+    String pName;
+    int qty;
+    double price;
 
-    public void addProduct(int cid, String customerName, String productName, int quantity, double price) {
+    
+    public void addProduct(int cid, String customerName, String productName, int quant, double price) {
         this.id = cid;
         this.cName = customerName;
         this.pName = productName;
-        this.qty = quantity;
+        this.qty = quant;
         this.price = price;
     }
 
+    
     public void viewProduct() {
         double totalDue = qty * price;
-        System.out.printf("%-10d %-10s %-10s %-10d %-10.2f %.2f\nTotal Due: %.2f\n",this.id, 
-                this.cName, this.pName, this.qty, this.price, totalDue);
+        System.out.printf("%-10d %-10s %-10s %-10d %-10.2f Total Due: %.2f\n ", this.id, 
+                          this.cName, this.pName, this.qty, this.price, totalDue);
     }
 
-    public void editProduct(String productName, int quantity, double price) {
+    
+    public void editProduct(String productName, int quant, double price) {
         this.pName = productName;
-        this.qty = quantity;
+        this.qty = quant;
         this.price = price;
     }
-
-    
 }
-    
--- src/it2d/bacus/Products.java
+++ src/it2d/bacus/Products.java
@@ -1,96 +1,110 @@
-
 package it2d.bacus;
 
 import java.util.Scanner;
 
 public class Products {
+    Product[] prod = new Product[100];
 
-    
-    
-    public void products(){
-        Scanner sc = new Scanner (System.in);  
-        Product[] prod = new Product[100];
+    public void products() {
+        Scanner sc = new Scanner(System.in);
         int nums = 0;
-        String choice;
-        do{
+        String choice; 
+        
+        do {
+            System.out.println("--------------------------");
+            System.out.println("WELCOME TO PRODUCT APP");
+            System.out.println("--------------------------");
+            System.out.println("1. ADD");
+            System.out.println("2. VIEW");
+            System.out.println("3. EDIT");
+            System.out.println("4. DELETE");
+            System.out.println("5. EXIT");
+            System.out.println("--------------------------");
 
-        System.out.println("--------------------------");
-        System.out.println("WELCOME TO Product APP");
-        System.out.println("--------------------------");
-        System.out.println("1. ADD");
-        System.out.println("2. VIEW");
-        System.out.println("3. EDIT");
-        System.out.println("4. DELETE");
-        System.out.println("5. EXIT");
-        System.out.println("--------------------------");
-        
-        System.out.print("Enter Option: ");
-        int option = sc.nextInt();
+            System.out.print("Enter your choice: ");
+            int option = sc.nextInt();
 
-        while(option > 5){
-            System.out.print("Invalid Selection, Try Again: ");
-            option = sc.nextInt();
-        }
-         switch(option){
-            
-        case 1:
-        System.out.print("Enter no. of Products: ");
-        nums = sc.nextInt();
+            while (option < 1 || option > 5) {
+                System.out.print("Invalid Choice, Try Again: ");
+                option = sc.nextInt();
+            }
 
-        for(int i=0; i < nums; i++){
-        System.out.print("Enter details of Product: "+(i+1));
-        
-        System.out.print("ID: ");
-        int id = sc.nextInt();
-        
-        System.out.print("Customer's name: ");
-        String cName = sc.nextLine();
-       
-        System.out.print("Product name: ");
-        String pName = sc.nextLine();
-        
-        System.out.print("Quantity: ");
-        int qty1 = sc.nextInt();
-        
-        System.out.print("Price: ");
-        double price1 = sc.nextInt();
-        
-        System.out.print("Cash: ");
-        double cash1 = sc.nextInt();
-        
-        prod[i] = new Product();
-        prod[i].addProduct(id, cName, pName, qty1, price1, cash1);
-        }
+            switch (option) {
+                case 1:
+                    
+                    System.out.print("Enter number of Products: ");
+                    nums = sc.nextInt();
+                    sc.nextLine();
 
-        break;
-        case 2:
-        for(int i=0; i < nums; i++){
-        prod[i].viewProduct();
-        }
-        break;
-        case 3:
+                    for (int i = 0; i < nums; i++) {
+                        System.out.println("Enter details of Product: " + (i + 1));
 
-        System.out.println("Enter ID of the Product: ");
-        int idin = sc.nextInt();
+                        System.out.print("ID: ");
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        
+                        System.out.print("Customer's name: ");
+                        String cName = sc.nextLine();
 
-        for (int i = 0; i < nums; i++) {
-        if (prod[i].id == idin) {
-        System.out.print("New Product: ");
-        double prelim = sc.nextDouble();
-        prod[i].editProducts(idin, Product);
-        break;
+                        System.out.print("Product name: ");
+                        String pName = sc.nextLine();
+
+                        System.out.print("Quantity: ");
+                        int qty1 = sc.nextInt();
+
+                        System.out.print("Price: ");
+                        double price1 = sc.nextDouble();
+
+                        prod[i] = new Product();
+                        prod[i].addProduct(id, cName, pName, qty1, price1);
                     }
-                }
-        break;
-        
-         }
-        System.out.println("Want to continue(yes/no)?: ");
-        choice = sc.next();
-        
-    
-         
-    }while(choice.equals("y") || choice.equals("y")); 
-        
-    }   
+                    break;
 
-    
+                case 2:
+                   
+                    if (nums == 0) {
+                        System.out.println("No products to display.");
+                    } else {
+                        for (int i = 0; i < nums; i++) {
+                            prod[i].viewProduct();
+                        }
+                    }
+                   
+                    break;
+
+                case 3:
+                 
+                    System.out.print("Enter ID of the Product to edit: ");
+                    int editID = sc.nextInt();
+                    
+
+                    for (int i = 0; i < nums; i++) {
+                        if (prod[i].id == editID) { 
+                            System.out.print("New Product name: ");
+                            sc.nextLine();
+                            String newName = sc.nextLine();
+
+                            System.out.print("New Quantity: ");
+                            int newQty = sc.nextInt();
+
+                            System.out.print("New Price: ");
+                            double newPrice = sc.nextDouble();
+
+                            prod[i].editProduct(newName, newQty, newPrice); 
+                            
+                            break;
+                        }
+                    }
+                    
+                    break;
+            }
+
+          
+            System.out.print("Want to continue (yes/no)?: ");
+            choice = sc.next();
+
+        } while (choice.equals("yes") || choice.equals("Yes"));
+
+       
+    }
+}
